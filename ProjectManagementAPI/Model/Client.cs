041ec48c126865bcb5ce316b5dffdf51150a2307
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2020-06-05 22:18:57 +0300

    Add project files.

---
+++ ProjectManagementAPI/Model/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementAPI.Model
+{
+    public class Client : IDbObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+
+        public List<Project> Projects { get; set; }
+
+        public IDbObject MakeNew()
+        {
+            return new Client {Name=Name,ImageUrl=ImageUrl };
+        }
+
+        public void UpdateFrom(IDbObject obj)
+        {
+            var q = obj as Client;
+            Name = q.Name;
+            ImageUrl = q.ImageUrl;
+        }
+    }
+}
+
---
+++ ProjectManagementAPI/Model/IDbObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementAPI.Model
+{
+    public interface IDbObject
+    {
+        int Id { get; }
+        IDbObject MakeNew();
+        void UpdateFrom(IDbObject obj);
+
+    }
+}
