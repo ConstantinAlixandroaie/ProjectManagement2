8eb977ff461406ef563eee20124d4802dea5548f
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2020-06-08 00:09:24 +0300

    ChecklistItem added.

--- ProjectManagementAPI/Model/CheckListItem.cs
+++ ProjectManagementAPI/Model/CheckListItem.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ProjectManagementAPI.Model
 {
-    public class CheckListItem
+    public class CheckListItem : IDbObject
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool StatusChecked { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateEdited { get; set; }
+        public int CheckListId { get; set; }
+
+        public CheckList CheckList { get; set; }
+
+
+        public IDbObject MakeNew()
+        {
+            return new CheckListItem { Text = Text, StatusChecked = StatusChecked, DateCreated = DateCreated, DateEdited = DateEdited, CheckListId = CheckListId };
+        }
+
+        public void UpdateFrom(IDbObject obj)
+        {
+            var q = obj as CheckListItem;
+            Text = q.Text;
+            StatusChecked = q.StatusChecked;
+            DateCreated = q.DateCreated;
+            CheckListId = q.CheckListId;
+        }
     }
 }
